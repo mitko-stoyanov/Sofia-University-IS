2bc1746ca5f031bff26192b50365f93eab0cbd4f
Mitko Stoyanov <mitko_st6@abv.bg>
2021-10-26 23:42:14 +0300

    Homework.

---
+++ UP-IS-2021-2022/week03/problem3_hw.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, doesExist;
+    string triangleType = "";
+    cout<<"Enter three numbers: ";
+    cin>>a>>b>>c;
+
+    doesExist = (a < b + c) &&
+                (b < a + c) &&
+                (c < a + b) &&
+                (a > c - b) &&
+                (b > c - a) &&
+                (c > b - a);
+
+    if(doesExist){
+        if(a == b && b == c){
+            triangleType = "Equilateral triangle.";
+        }
+        else if(a == b || b == c || a == c){
+            triangleType = "Isosceles triangle.";
+        }
+        else if (a != b && b != c && a != c)
+        {
+            triangleType = "Scalene triangle.";
+        }
+
+        cout<<"The triangle exist. "<<triangleType<<endl;
+    }
+    else{
+        cout<<"The triangle does not exist!"<<endl;
+    }
+
+    return 0;
+}
