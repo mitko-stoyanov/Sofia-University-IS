a6f587cfc6352ea5f649d126003483dd602ff565
Mitko Stoyanov <88737715+mitko-stoyanov@users.noreply.github.com>
2021-10-29 11:28:31 +0300

    Update problem3_hw.cpp

--- UP-IS-2021-2022/week03/problem3_hw.cpp
+++ UP-IS-2021-2022/week03/problem3_hw.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main()
 {
-    int a, b, c, doesExist;
+    int a, b, c;
+    bool doesExist = false;
     string triangleType = "";
     cout<<"Enter three numbers: ";
     cin>>a>>b>>c;
