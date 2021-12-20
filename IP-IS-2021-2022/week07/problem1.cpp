c288fcbe33fa1fd2b1c0916ff7c4dd796990bf4e
Mitko Stoyanov <mitko_st6@abv.bg>
2021-12-20 23:27:11 +0200

    Classwork.

---
+++ IP-IS-2021-2022/week07/problem1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void evenNumbersFromArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i) {
+        if (arr[i] % 2 == 0) {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // First Task
+    return 0;
+}
