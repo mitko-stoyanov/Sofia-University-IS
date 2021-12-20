4b8d2ecce3010de5e0b6246f2970578c5786b82b
Mitko Stoyanov <mitko_st6@abv.bg>
2021-12-20 23:36:05 +0200

    Classwork.

---
+++ IP-IS-2021-2022/week07/problem6.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void multiplyIntArray(int arr[], int n, double k) {
+    for (int i = 0; i < n; ++i) {
+        arr[i] *= k;
+    }
+}
+
+void multiplyDoubleArray(double arr[], int n, double k) {
+    for (int i = 0; i < n; ++i) {
+        arr[i] *= k;
+    }
+}
+
+int main()
+{
+    // Sixth Task
+    return 0;
+}
