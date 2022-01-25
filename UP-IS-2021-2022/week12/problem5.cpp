83689c483199cc5d967543fd9416d0848105796f
Mitko Stoyanov <mitko_st6@abv.bg>
2022-01-25 17:35:22 +0200

    Homework.

---
+++ UP-IS-2021-2022/week12/problem5.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+    if(n == 0 || n == 1){
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+
+}
+
+int main(){
+    int n;
+    cout<<"Enter which element do you want to see?: ";
+    cin>>n;
+    cout<<"The fibonacci element is: "<<fibonacci(n)<<endl;
+    return 0;
+}
