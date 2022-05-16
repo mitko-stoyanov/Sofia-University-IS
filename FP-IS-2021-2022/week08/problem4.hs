7443a48f1c9079788bfa7a800a8a0fef3128522e
Mitko Stoyanov <mitko_st6@abv.bg>
2022-05-16 23:42:15 +0300

    Classwork.

---
+++ FP-IS-2021-2022/week08/problem4.hs
@@ -0,0 +1,15 @@
+main :: IO()
+main = do
+    print $ factorize 152 == [2, 2, 2,19]
+    print $ factorize 123 == [3, 41]
+    print $ factorize 13 == [13]
+    -- print $ factorize 152954 -- == [13]
+
+factorize :: Int -> [Int]
+factorize n = helper n 2
+ where
+     helper :: Int -> Int -> [Int]
+     helper 1 _ = []
+     helper leftover d
+      | mod leftover d == 0 = d : helper (div leftover d) d
+      | otherwise = helper leftover (d + 1)
